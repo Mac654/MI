e2a0b27915c383834ede23de43bf352e2fd1d7bd
Marjan Petrushevski <pro-gaming@live.com>
2022-12-02 21:09:09 +0100

    test123

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={'/quotes'} element={<Quotes/>} />
+        <Route path={'/'} element={<Quotes/>} />
         <Route path={'/random-quote'} element={<RandomQuote/>}/>
       </Routes>
     </div>
